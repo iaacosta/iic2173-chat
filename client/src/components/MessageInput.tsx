b578333b19e9df3997d8e38593a9b832ee42e29b
Nacho <iaacosta@uc.cl>
2019-09-01 13:52:06 -0400

    🚨 remove linter warning

--- client/src/components/MessageInput.tsx
+++ client/src/components/MessageInput.tsx
@@ -1,10 +1,9 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
-import dayjs from 'dayjs';
 
 import { colors, rgba } from 'lib/styles';
-import { Message } from 'lib/types';
 
 const InputArea = styled.div`
   grid-area: sender;
