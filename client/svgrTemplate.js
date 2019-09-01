1fe18c18c0252eb996f8b4088557aa9444f2a9c6
Nacho <iaacosta@uc.cl>
2019-09-01 01:10:13 -0400

    ➕ add svgr for generating react svgs

---
+++ client/svgrTemplate.js
@@ -0,0 +1,13 @@
+module.exports = function template({ template }, _, { componentName, jsx }) {
+  const typeScriptTpl = template.smart({
+    plugins: ['typescript'],
+  });
+
+  return typeScriptTpl.ast`
+      import React from 'react';
+  
+      const ${componentName} = (props: React.SVGProps<SVGSVGElement>) => ${jsx};
+      
+      export default ${componentName};
+    `;
+};
