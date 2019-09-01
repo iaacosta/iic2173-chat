dd12f5553f64bad70c8063967e88fd6428d9708d
Nacho <iaacosta@uc.cl>
2019-09-01 01:16:52 -0400

    🚨 remove cra linter warning

--- client/src/lib/helpers/common.ts
+++ client/src/lib/helpers/common.ts
@@ -2,7 +2,7 @@ export const hyphenate = (str: string, max: number) => {
   const newString: string[] = [];
 
   Array.from(str).forEach((char, i) => {
-    if (i % (max - 1) !== 0 || i == 0) newString.push(char);
+    if (i % (max - 1) !== 0 || i === 0) newString.push(char);
     else {
       newString.push('-\n');
       newString.push(char);
