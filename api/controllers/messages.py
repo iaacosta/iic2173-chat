3cb5dcecba62204f4a1400040438da5cdb472557
Nacho <iaacosta@uc.cl>
2019-12-14 20:09:08 -0300

    ✨ add base lambda function request

--- api/controllers/messages.py
+++ api/controllers/messages.py
@@ -1,4 +1,6 @@
 import json
+import os
+import boto3
 from datetime import datetime
 from flask import Blueprint, render_template, request, abort
 from flask_cors import cross_origin
@@ -37,3 +39,19 @@ def get_messages():
         'user': t[2],
         'content': t[3]}, message.all())
     return json.dumps(list(messages))
+
+
+@messages_blueprint.route('/test')
+@cross_origin()
+def test():
+    client = boto3.client(
+        'lambda',
+        aws_access_key_id=os.environ['AWS_IAM_ID'],
+        aws_secret_access_key=os.environ['AWS_IAM_SECRET'],
+        region_name=os.environ['AWS_REGION']
+    )
+
+    response = client.invoke(
+        FunctionName="exampleFunction", InvocationType="RequestResponse")
+
+    return response["Payload"].read().decode('utf-8')
