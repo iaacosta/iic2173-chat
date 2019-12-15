9bb90a8b9b1ad112b685895100f5b01b2ca539a1
Nacho <iaacosta@uc.cl>
2019-12-15 14:46:45 -0300

    :sparkles: add dynamic routing depending on FLASK_ENV

--- api/controllers/messages.py
+++ api/controllers/messages.py
@@ -8,6 +8,12 @@ from database import get_database
 from models import Message
 
 messages_blueprint = Blueprint('messages', __name__)
+aws_client = boto3.client(
+    'lambda',
+    aws_access_key_id=os.environ['AWS_IAM_ID'],
+    aws_secret_access_key=os.environ['AWS_IAM_SECRET'],
+    region_name='us-east-1'
+)
 
 
 @messages_blueprint.route('', methods=['POST'])
@@ -21,8 +27,19 @@ def post_message():
         abort(400)
 
     try:
-        message = Message(content, user)
-        message.save()
+        if os.environ['FLASK_ENV'] != 'production':
+            message = Message(content, user)
+            message.save()
+        else:
+            if user is None:
+                user = 'Anónimo'
+
+            lambda_res = aws_client.invoke(
+                FunctionName="arn:aws:lambda:us-east-1:272559496018:function:createMessage",
+                Payload=bytearray(json.dumps(
+                    {'content': content, 'user': user}), encoding='utf-8')
+            )
+
         return 'OK'
     except Exception as e:
         abort(500)
@@ -31,11 +48,30 @@ def post_message():
 @messages_blueprint.route('/')
 @cross_origin()
 def get_messages():
-    message = Message()
-    messages = map(lambda t: {
-        'id': t[0],
-        'date': int(datetime.strptime(t[1], '%Y-%m-%d %H:%M:%S').timestamp()
-                    * 1000),
-        'user': t[2],
-        'content': t[3]}, message.all())
-    return json.dumps(list(messages))
+    if os.environ['FLASK_ENV'] != 'production':
+        message = Message()
+        messages = map(lambda t: {
+            'id': t[0],
+            'date': int(
+                datetime.strptime(t[1], '%Y-%m-%d %H:%M:%S').timestamp() * 1000
+            ),
+            'user': t[2],
+            'content': t[3]}, message.all())
+        return json.dumps(list(messages))
+    else:
+        lambda_res = aws_client.invoke(
+            FunctionName="arn:aws:lambda:us-east-1:272559496018:function:getMessages")
+
+        payload = json.loads(lambda_res["Payload"].read().decode('utf-8'))
+        parsed_data = map(lambda t: {
+            'id': t['id'],
+            'date': int(
+                datetime.strptime(
+                    t['date'],
+                    '%a %b %d %Y %H:%M:%S GMT+0000 (Coordinated Universal Time)'
+                ).timestamp() * 1000
+            ),
+            'user': t['user'],
+            'content': t['content']}, payload['body']['messages'])
+
+        return json.dumps(list(parsed_data))
